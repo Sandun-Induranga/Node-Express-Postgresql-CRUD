2536c32bfdb4204206161e1430b072a049c41a39
Sandun Induranga <sandutharu40@gmail.com>
2023-07-19 20:42:36 +0530

    Express Server Created

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,10 @@
-console.log("Hello World!");
+import express from 'express';
+import dotenv, { config } from 'dotenv';
+config();
+
+const app = express();
+
+app.listen(process.env.PORT , () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+    }
+);
