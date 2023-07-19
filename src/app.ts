fcb1b5ac151edf21eb4616f4694d10d37632d544
Sandun Induranga <sandutharu40@gmail.com>
2023-07-19 22:42:53 +0530

    Save Customer

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,13 @@ pool.query('SELECT * FROM "Customer".customer', (err, res) => {
     pool.end();
 });
 
+const query = ('Insert into "Customer".customer values ($1, $2, $3, $4) returning *');
+const values = ['C002', 'John', 'England', 1000];
+pool.query(query, values, (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
     }
