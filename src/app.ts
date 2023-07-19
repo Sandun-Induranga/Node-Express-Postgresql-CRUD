ab363c38667f46a2984b3082d252712646368f9e
Sandun Induranga <sandutharu40@gmail.com>
2023-07-19 22:56:18 +0530

    Updatee Customer

--- src/app.ts
+++ src/app.ts
@@ -7,12 +7,15 @@ const app = express();
 
 pool.query('SELECT * FROM "Customer".customer', (err, res) => {
     console.log(err, res);
-    pool.end();
 });
 
 const query = ('Insert into "Customer".customer values ($1, $2, $3, $4) returning *');
-const values = ['C002', 'John', 'England', 1000];
+const values = ['C003', 'Ramal', 'Colombo', 2000];
 pool.query(query, values, (err, res) => {
+    console.log(err, res);
+});
+
+pool.query('UPDATE "Customer".customer SET cusName = $1 WHERE cusId = $2 returning *', ['Dasun', 'C003'], (err, res) => {
     console.log(err, res);
     pool.end();
 });
--- src/db/db.ts
+++ src/db/db.ts
@@ -6,7 +6,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     host: process.env.DB_HOST,
     database: process.env.DB_NAME,
-    password: process.env.DB_PASS,
+    password: String(process.env.DB_PASS),
     port: Number(process.env.DB_PORT),
 });
 
