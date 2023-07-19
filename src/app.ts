0658b128a6e9bf1968a8768e2090ddaa87777605
Sandun Induranga <sandutharu40@gmail.com>
2023-07-19 22:39:02 +0530

    GetAllData

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
-import dotenv, { config } from 'dotenv';
+import { config } from 'dotenv';
+import pool from './db/db';
 config();
 
 const app = express();
 
-app.listen(process.env.PORT , () => {
+pool.query('SELECT * FROM "Customer".customer', (err, res) => {
+    console.log(err, res);
+    pool.end();
+});
+
+app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
     }
 );
